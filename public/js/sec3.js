2a546b690ef4e1838ec02c3e0f4486decb72e4d9
AngeloDima <Angelodimauro91@gmail.com>
2024-05-26 15:51:38 +0200

    Carosello correggo le info

--- public/js/sec3.js
+++ public/js/sec3.js
@@ -1,6 +1,6 @@
 let arrayChiaveValore = [
-    { Titolo: "Titolo1", Descrizione: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id architecto earum debitis. Quod veritatis laboriosam vitae. Voluptatem, id veritatis ut, beatae, debitis facilis ex quod voluptate unde animi iure!", Features: "Features1", Dimensions: "Dimensions1", Price: "100", img1: "https://shop.citybeachboardshop.com/prodotti/15483/XXL/15483foto.jpg", img2: "https://shop.citybeachboardshop.com/prodotti/13074/XXL/13074foto.jpg", img3: "https://shop.citybeachboardshop.com/prodotti/16338/XXL/16338foto.jpg" },
-    { Titolo: "Titolo2", Descrizione: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id architecto earum debitis. Quod veritatis laboriosam vitae. Voluptatem, id veritatis ut, beatae, debitis facilis ex quod voluptate unde animi iure!", Features: "Features2", Dimensions: "Dimensions2", Price: "200", img1: "https://img.freepik.com/free-photo/raccoon_649448-3855.jpg", img2: "https://media.istockphoto.com/id/514622028/it/foto/piccolo-su-albero-di-procione.jpg?s=612x612&w=0&k=20&c=v-XYkYMyHugalZKoNTOiuc8ITjy_W8y1r3cVed1tV0o=", img3: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2B1hpyZH2XXzAUynEvCa4qm0m_8Ze5PopXcqOcLGSww&s" },
+    { Titolo: "JR Surfboards The Donny Stoker Yellow/Green Rail Fade", Descrizione: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id architecto earum debitis. Quod veritatis laboriosam vitae. Voluptatem, id veritatis ut, beatae, debitis facilis ex quod voluptate unde animi iure!", Features: "Features1", Dimensions: "Dimensions1", Price: "100", img1: "http://lepistina.com/projects/SurfsCo/img/surf-board-1.png", img2: "https://shop.citybeachboardshop.com/prodotti/13074/XXL/13074foto.jpg", img3: "https://shop.citybeachboardshop.com/prodotti/16338/XXL/16338foto.jpg" },
+    { Titolo: "Surfboards The Big Heart Red/Brown Rail Fade", Descrizione: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id architecto earum debitis. Quod veritatis laboriosam vitae. Voluptatem, id veritatis ut, beatae, debitis facilis ex quod voluptate unde animi iure!", Features: "Features2", Dimensions: "Dimensions2", Price: "200", img1: "https://img.freepik.com/free-photo/raccoon_649448-3855.jpg", img2: "https://media.istockphoto.com/id/514622028/it/foto/piccolo-su-albero-di-procione.jpg?s=612x612&w=0&k=20&c=v-XYkYMyHugalZKoNTOiuc8ITjy_W8y1r3cVed1tV0o=", img3: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2B1hpyZH2XXzAUynEvCa4qm0m_8Ze5PopXcqOcLGSww&s" },
 
 ];
 
@@ -26,12 +26,14 @@ function mostraElementoCorrente(option) {
     descrizioneElement.textContent = option === "Description" ? element.Descrizione : "";
     featuresElement.textContent = option === "Features" ? element.Features : "";
     dimensionsElement.textContent = option === "Dimensions" ? element.Dimensions : "";
-    priceElement.textContent = element.Price.toString();
+    // Aggiungi il simbolo "$" prima del prezzo
+    priceElement.textContent = '$' + element.Price.toString();
     img1Element.src = element.img1;
     img2Element.src = element.img2;
     img3Element.src = element.img3;
     indexElement.textContent = `${currentIndex + 1}`+ '/' + arrayChiaveValore.length;
 }
+
 
 function swapImages(imgElement1, imgElement2) {
     const tempSrc = imgElement1.src;
--- resources/views/HomePage/Sec3.blade.php
+++ resources/views/HomePage/Sec3.blade.php
@@ -23,7 +23,7 @@
 
 
         <div class="dx">
-            <div class="position-relative">
+            <div class="position-relative mb-dima">
                 <h1 id="Titolo"></h1>
 
                 <div class="d-flex gap-Custom borderCustom">
@@ -42,8 +42,10 @@
                 <button id="btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></button>
                 <button id="btn2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></button>
                 <div class="Index" id="Index"></div>
+
+
             </div>
-            <a href="">View All Boards</a>
+            <a href="" id="info">View all boards</a>
         </div>
 
         <script src="{{ asset('js/sec3.js') }}"  crossorigin="anonymous"></script>
