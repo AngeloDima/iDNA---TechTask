9939094682b15d060818aafed97280c863091e9f
AngeloDima <Angelodimauro91@gmail.com>
2024-05-27 02:27:31 +0200

    Fix carosello

--- resources/views/HomePage/Sec6.blade.php
+++ resources/views/HomePage/Sec6.blade.php
@@ -9,6 +9,13 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam esse ipsum tempore amet nemo iusto deserunt vel enim dolor optio adipisci, odit eos laboriosam aut ad id ipsa quae aliquid. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio nihil consectetur a sunt eos? Vel et accusantium similique sapiente. Ab consectetur</p>
             <a href="">Read more</a>
         </div>
-        <div class="dx"></div>
+        {{-- <div class="dx">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+        </div> --}}
+
+        <div class="dx">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/h7DK3h4RI-s?si=IIufm4qKDlGhvuQl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+
     </div>
 </body>
